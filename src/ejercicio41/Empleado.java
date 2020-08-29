8b79818a38e82fee81dff259627cc367cb448a51
Leocristalli12 <39451818+Leocristalli12@users.noreply.github.com>
2020-08-29 18:18:49 -0300

    Ejercicio 12 first commit

---
+++ src/ejercicio41/Empleado.java
@@ -0,0 +1,25 @@
+package ejercicio41;
+
+public class Empleado {
+	private String nombre, apellido;
+	private int totalHoras;
+	public int getTotalHoras() {
+		return totalHoras;
+	}
+	public void setTotalHoras(int totalHoras) {
+		this.totalHoras = totalHoras;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+}
